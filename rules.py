e000109573694f6cc44661df4f83992b31e78d44
Fyodor Sizov <f.sizov@yandex.ru>
2017-11-07 23:33:16 +0300

    ++ parent layer switching

--- rules.py
+++ rules.py
@@ -2,6 +2,7 @@
 import grammar
 # ...
 # first we should check if the whole token exists in dictionaries
+# parent layer switching due to [time limits] like universal:token < [universal:input -> mansi:SH_MOD]
 #
 rombandeeva = grammar.Container()
 
