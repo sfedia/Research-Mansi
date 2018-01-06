3fec79aa221ff1a7e0623666bd6beda0aef79707
Fyodor Sizov <f.sizov@yandex.ru>
2018-01-06 16:34:25 +0300

    update

--- VakhrParsing/VakhrSplit.py
+++ VakhrParsing/VakhrSplit.py
@@ -2,3 +2,29 @@
 
 import re
 
+bal_vakhr = open('balandin_vakhr.txt', encoding='utf-8').read().splitlines()
+
+pre_buffer = ''
+post_buffer = ''
+
+class_cmcc = r'[А-ЯӒЁӇӦӰӘӚ]'
+class_smcc = r'[а-яӓёӈӧӱәӛ]'
+class_smcc_n_punct = r'[а-яӓёӈӧӱәӛ\,-\s]'
+
+len_bv = len(bal_vakhr)
+
+
+def regex_for_lines(line_number, regexp):
+    regexp = re.sub(r'\\n(.*?)\\n', r'\n^\g<1>$\n', regexp)
+    lines_regex = regexp.split(r'\n')
+    for incr, content in bal_vakhr[line_number:]:
+        if incr == len(regexp):
+            break
+        if (line_number + incr) == (len_bv - 1) and incr < (len(regexp) - 1):
+            return False
+        if not re.search(lines_regex[incr], content):
+            return False
+
+
+for line in bal_vakhr:
+    print(line)
