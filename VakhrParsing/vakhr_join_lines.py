d9c86c0ec24b833a6c0f521a366c22882d63aa01
Fyodor Sizov <f.sizov@yandex.ru>
2018-01-08 13:39:45 +0300

    join lines

---
+++ VakhrParsing/vakhr_join_lines.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import re
+import is_russian
+
+bal_vakhr = open('balandin_vakhr_3.txt', encoding='utf-8').read().splitlines()
+checker = is_russian.Checker(import_op=False)
+len_bv = len(bal_vakhr)
+new_bv = []
+
+for e, line in enumerate(bal_vakhr):
+    join = False
+    if 0 < e < len_bv - 1:
+        check_stack = [bal_vakhr[e - 1].split()[0], line.split()[0], bal_vakhr[e + 1].split()[0]]
+        sorted_stack = check_stack[:]
+        sorted_stack.sort()
+        if check_stack != sorted_stack:
+            join = True
+    if e > 0:
+        if re.search(r'\s*-\s*$', line):
+            join = True
+        elif re.search(r'\s*\d+[\.,]\s*$', line):
+            join = True
+        elif re.search(r'\s*,\s*$', line):
+            join = True
+
+    if join:
+        new_bv[-1] += ' ' + line
+        new_bv[-1] = re.sub(r'\s{2,}', ' ', new_bv[-1])
+    else:
+        new_bv.append(line)
+
+with open('balandin_vakhr_4.txt', 'w', encoding='utf-8') as new_bv_file:
+    new_bv_file.write("\n".join(new_bv))
+    new_bv_file.close()
