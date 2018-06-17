051814ff9f68180677350feb177a48689c7d4c58
Fyodor Sizov <f.sizov@yandex.ru>
2018-06-17 23:15:42 +0300

    added ArchivePage object

--- luima_seripos/parser.py
+++ luima_seripos/parser.py
@@ -2,6 +2,18 @@
 
 import re
 import requests
+import lxml.html
 
 
-...
+class ArchivePage:
+    def __init__(self, archive_url):
+        self.ap_request = requests.get(archive_url)
+        self.page_code = self.ap_request.text
+        self.links = self.__get_no_links()
+
+    def __get_no_links(self):
+        ls_prefix = "http://www.khanty-yasang.ru"
+        a_link_path = ".view-content table tbody tr td div div a"
+        page_html = lxml.html.fromstring(self.page_code)
+        a_links = page_html.cssselect(a_link_path)
+        return [ls_prefix + link.get('href') for link in a_links]
