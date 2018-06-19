b9cf10730586cd58172ba8cd9d318edd9c459ac6
Fyodor Sizov <f.sizov@yandex.ru>
2018-06-19 16:12:17 +0300

    support making capsules

--- luima_seripos/decode_pdf_text.py
+++ luima_seripos/decode_pdf_text.py
@@ -12,15 +12,40 @@ class FileDecode:
         self.file_path = file_path
         self.file_name = file_name
         self.saving_folder = saving_folder
+        self.capsules = []
         self.text = open(self.file_path + '/' + self.file_name, encoding='utf-8').read()
 
     def format_text(self):
         self.remove_i()
+        self.make_capsules()
         self.change_encoding()
         self.regular_correction()
+        self.unpack_capsules()
 
     def remove_i(self):
         self.text = re.sub(r'\nI+\n', '\n', self.text)
+
+    def key_gen(self, number=None):
+        return "%~$" + str(len(self.capsules) if number is None else number) + "$~%"
+
+    def make_capsules(self):
+        regex_list = []
+        for regex in regex_list:
+            found = [x.group(0) for x in re.finditer(regex, self.text)]
+            for occurrence in found:
+                self.text = self.text.replace(occurrence, self.key_gen())
+                self.capsules.append(occurrence)
+
+    def unpack_capsules(self):
+        for j, capsule in enumerate(self.capsules):
+            self.text = self.text.replace(self.key_gen(j), self.capsules[j])
+
+    def pack_capsule(self, regex):
+        key = "%~$" + str(len(self.capsules)) + "$~%"
+        found = [x.group(0) for x in re.finditer(regex, self.text)]
+        for occurrence in found:
+            self.text = self.text.replace(occurrence, key)
+
 
     def change_encoding(self):
         time.sleep(0.1)
