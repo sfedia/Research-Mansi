e6241551b59a755e1db4cc7abd60ebe7b6ab0528
Fyodor Sizov <f.sizov@yandex.ru>
2018-06-24 18:49:36 +0300

    update parser

--- VakhrParsing2/parser.py
+++ VakhrParsing2/parser.py
@@ -20,6 +20,9 @@ class Dictionary:
             if obj.object_type == obj_type:
                 return obj
 
+    def append(self, obj):
+        self.independent_objects.append(obj)
+
 
 class ParsingObject:
     def __init__(self, content, object_type):
@@ -35,3 +38,23 @@ class ParsingObject:
         self.content += content2insert
         if update_function is not None:
             self.content = update_function(self.content)
+
+
+class ObjectGrouping:
+    def __init__(self, object_type):
+        self.object_type = object_type
+        self.groups = []
+
+    def get_last(self):
+        if self.groups:
+            return self.groups[-1].get_last()
+
+    def get_last_by_type(self, object_type):
+        if object_type == self.object_type:
+            return self.get_last()
+
+    def connect(self, object2connect):
+        self.get_last().connect(object2connect)
+
+    def insert_content(self, content2insert, update_function=None):
+        self.get_last().insert_content(content2insert, update_function)
