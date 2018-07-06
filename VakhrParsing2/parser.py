4d263129c3696b1c3a2f0a5fdd54749a5c10447a
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-06 22:35:38 +0300

    new .dive() recursive method

--- VakhrParsing2/parser.py
+++ VakhrParsing2/parser.py
@@ -27,6 +27,18 @@ class ParsingObject:
     def connect(self, object2connect):
         self.connected_objects.append(object2connect)
 
+    def dive(self, counter=1, condition=lambda x: True):
+        for obj in reversed(self.connected_objects):
+            if condition(obj):
+                counter -= 1
+            if counter < 0:
+                break
+            elif not counter:
+                return self, 0
+            found_child, counter = obj.dive(counter, condition)
+            if found_child is not None:
+                return found_child, counter
+        return None, counter
 
     def insert_content(self, content2insert, update_function=None):
         self.content += content2insert
